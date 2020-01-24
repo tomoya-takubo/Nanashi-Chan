0c739a2b4abb7b0954d6eac623e490eaa97ccbc4
tomoya-takubo <tkb1003@gmail.com>
2020-01-25 01:06:46 +0900

    初回コミット
    Nanashi-Chanプロジェクト作成～タイトルステージまで

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// クラスの概要をここに書くことができるよ！
+/// </summary>
+public class Title : MonoBehaviour
+{
+    private bool firstPush = false;
+    
+    /// <summary>
+    /// ボタンを押すとシーンが変わるよ！
+    /// unity上のボタンで登録済み（クラス間での参照ではないので１行下の参照にカウントされないため記載します
+    /// </summary>
+    public void PressStart()
+    {
+        Debug.Log("Press Start!");
+
+        if (!firstPush)
+        {
+            Debug.Log("Go Next Scene!");
+            //ここに次のシーンへいく命令を書く
+
+            //
+            firstPush = true;
+        }
+    }
+}
